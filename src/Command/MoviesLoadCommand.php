ad3abdf33f65cdeaef7dd0fe5855ab1e192cc2c5
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-10-12 17:20:00 +0200

    Add table on movie loader command

--- src/Command/MoviesLoadCommand.php
+++ src/Command/MoviesLoadCommand.php
@@ -9,6 +9,8 @@ use App\Repository\GenreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -46,12 +48,21 @@ class MoviesLoadCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $output->getFormatter()->setStyle('success', new OutputFormatterStyle('black', 'green'));
 
+        $displayTable = $output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE;
+        if ($displayTable) {
+            $table = new Table($output->section());
+            $table->setHeaders(['Id', 'Title', 'Rating', 'Genres', 'ReleaseDate']);
+            $table->render();
+        }
+
+        $notLoadedMovieIds = [];
         foreach (self::MOVIE_IDS as $id) {
             try {
                 $imdbMovie = $this->movieProvider->getById($id);
             } catch (\Exception $e) {
-                $io->warning(sprintf('Movie %s not loaded.', $id));
+                $notLoadedMovieIds[] = $id;
                 continue;
             }
 
@@ -72,12 +83,39 @@ class MoviesLoadCommand extends Command
             }
 
             $this->entityManager->persist($movie);
+
+            if ($displayTable) {
+                $table->appendRow([
+                    $id,
+                    $movie->getTitle(),
+                    $this->formatRating($movie->getRating(), $output),
+                    implode(', ', $movie->getGenres()->toArray()),
+                    $movie->getReleaseDate()->format(\DateTimeInterface::RFC822),
+                ]);
+            }
         }
 
         $this->entityManager->flush();
 
+        foreach ($notLoadedMovieIds as $id) {
+            $io->warning(sprintf('Movie %s not loaded.', $id));
+        }
+
         $io->success('Movies successfully loaded in database');
 
         return Command::SUCCESS;
+    }
+
+    private function formatRating(int $rating, OutputInterface $output): string
+    {
+        $ratingFormatted = number_format($rating / 10, 1);
+
+        if ($ratingFormatted >= 8) {
+            $ratingFormatted = sprintf('<success>%s</>', $ratingFormatted);
+        } elseif ($ratingFormatted < 5) {
+            $ratingFormatted = sprintf('<error>%s</>', $ratingFormatted);
+        }
+
+        return $ratingFormatted;
     }
 }
