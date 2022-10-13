1d9566461b93da4a1ee9150b310c8dc4d947992a
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2022-10-13 16:48:30 +0200

    Add remember me feature

--- src/EventListener/SaveLastLoginListener.php
+++ src/EventListener/SaveLastLoginListener.php
@@ -4,17 +4,25 @@ namespace App\EventListener;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 #[AsEventListener(InteractiveLoginEvent::class)]
 final class SaveLastLoginListener
 {
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private AuthorizationCheckerInterface $authorizationChecker,
+    )
     {
     }
 
     public function __invoke(InteractiveLoginEvent $event): void
     {
+        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return;
+        }
+
         $event->getAuthenticationToken()->getUser()->setLastLogin(new \DateTimeImmutable());
         $this->entityManager->flush();
     }
